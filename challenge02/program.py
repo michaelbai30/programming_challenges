a65a855dc69f0ce7e9ce3537c4d91022c2062698
Michael Bai <mbai@nd.edu>
2022-09-03 01:24:26 -0400

    challenge02: done

--- challenge02/program.py
+++ challenge02/program.py
@@ -1,19 +1,18 @@
 import sys
-def isValidTag(s):
+def is_valid_tag(input_string):
     stack = []
 
-    for i, char in enumerate(s):
+    for i, char in enumerate(input_string):
         if char == '<':
 
             # index of '<' and '>' respectively
-            openIndex = i
-            closedIndex = s.find('>', openIndex)
-            
+            open_index = i
+            closed_index = input_string.find('>', open_index)
             # if the character after the '<' is a '/' (represents closing tag)
-            if s[openIndex + 1] == '/':
+            if input_string[open_index + 1] == '/':
 
                 # if stack is not empty and the top of the stack is equal to the the contents inside <...>
-                if stack and stack[-1] == s[openIndex + 2:closedIndex]:
+                if stack and stack[-1] == input_string[open_index + 2:closed_index]:
                     stack.pop()
                 else:
                     return 'Unbalanced'
@@ -21,16 +20,16 @@ def isValidTag(s):
 
                 # the tag is an opening tag
                 # add the string inside < > to the stack 
-                stack.append(s[openIndex+1:closedIndex])
+                stack.append(input_string[open_index+1:closed_index])
 
             # sliding window ensures algorithm is O(n)
-            i = closedIndex
+            i = closed_index
 
     return 'Balanced' if not stack else 'Unbalanced'
 
 def main():
     for line in sys.stdin:
-        print(isValidTag(line))
+        print(is_valid_tag(line))
         
 if __name__ == '__main__':
     main()
