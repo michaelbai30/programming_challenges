6de444decf0393db0cd2b8ed5a3ed90c816bd1c5
Michael Bai <mbai@nd.edu>
2022-09-14 14:48:42 -0400

    challenge05: done

--- challenge05/program.py
+++ challenge05/program.py
@@ -1,6 +1,6 @@
 import sys
 
-def get_collatz_cycle_length(num, collatz_cyc_len_dict):
+def calc_collatz_cycle_length(num, collatz_cyc_len_dict):
     # special case: if num is already 1, just return 1
     if num == 1:
         return 1
@@ -9,15 +9,17 @@ def get_collatz_cycle_length(num, collatz_cyc_len_dict):
     if num not in collatz_cyc_len_dict:
         if num % 2 == 1:
             # recursive call (3n + 1)
-            cyc_len = get_collatz_cycle_length(3*num + 1, collatz_cyc_len_dict)
+            cyc_len = calc_collatz_cycle_length(3*num + 1, collatz_cyc_len_dict)
             collatz_cyc_len_dict[num] = cyc_len + 1
             return collatz_cyc_len_dict[num]
         else:
             # recursive call (n // 2)
-            cyc_len = get_collatz_cycle_length(num//2, collatz_cyc_len_dict)
+            cyc_len = calc_collatz_cycle_length(num//2, collatz_cyc_len_dict)
             collatz_cyc_len_dict[num] = cyc_len + 1
             return collatz_cyc_len_dict[num]
-            
+
+    # MEMOIZATION
+    # if num already in dict, we don't need to recompute it, just return the value.      
     else:
         return collatz_cyc_len_dict[num]
 
@@ -25,7 +27,7 @@ def get_collatz_cycle_length(num, collatz_cyc_len_dict):
 def get_max_cycle_len(begin, end, collatz_cyc_len_dict):
     max_cycle_len = 0
     while begin <= end:
-        cycle_len = get_collatz_cycle_length(begin, collatz_cyc_len_dict)
+        cycle_len = calc_collatz_cycle_length(begin, collatz_cyc_len_dict)
         # if curr cycle_len beats max_cycle_len, update max
         max_cycle_len = max(cycle_len, max_cycle_len)
         begin+= 1
@@ -41,9 +43,9 @@ store_memo = {}
 num_arr = []
 for line in sys.stdin:
     elements=line.split()
-    begin = int(elements[0])
-    end = int(elements[1])
-    num_arr.append([begin, end])
+    begin_seq = int(elements[0])
+    end_seq = int(elements[1])
+    num_arr.append([begin_seq, end_seq])
 
 for intervals in num_arr:
     if intervals[0] > intervals[1]:
@@ -51,8 +53,7 @@ for intervals in num_arr:
         # results[1] = the max cycle len
         results = get_max_cycle_len(intervals[1], intervals[0], store_memo)
     else:
-       results = get_max_cycle_len(intervals[0], intervals[1], store_memo) 
+        results = get_max_cycle_len(intervals[0], intervals[1], store_memo) 
     
     print(f"{intervals[0]} {intervals[1]} {results[0]} {results[1]}") 
     store_memo.clear()
-
