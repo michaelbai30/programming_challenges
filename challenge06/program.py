cfa4c0cc8d84e3de6e0800eb64b2150b682e7d38
Michael Bai <mbai@nd.edu>
2022-09-16 18:46:04 -0400

    challenge06: done

---
+++ challenge06/program.py
@@ -0,0 +1,36 @@
+import sys
+
+def distinct_fruit(fruits):
+
+    hash_table = {}
+    curr_len = 0
+    max_len = 0 
+    max_seq = ''
+
+    # starting position of substring containing the max len seq of distinct fruit
+    start = -1 # default = -1
+
+    for i, fruit in enumerate(fruits):
+        if fruit in hash_table and hash_table[fruit] > start: # if already hashed and new start index is farther along than curr start
+            start = hash_table[fruit] # update start
+        
+        hash_table[fruit] = i # add to hash, even if already hashed
+        curr_len = i - start 
+        if curr_len > max_len: # new max_seq
+            max_seq = fruits[start + 1 : i + 1]
+        max_len = max(max_len, curr_len)
+
+    return max_len, max_seq
+
+def main():
+    fruits_list = [] # list of test cases
+
+    for line in sys.stdin:
+        fruits = line.strip().split(' ')
+        fruits_list.append(fruits)
+    
+    for i, fruits in enumerate(fruits_list):
+        print(f'{distinct_fruit(fruits)[0]}: {", ".join(distinct_fruit(fruits)[1])}')
+
+if __name__ == '__main__':
+    main()
