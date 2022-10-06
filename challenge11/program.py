b153f57728e5e18601c731fc8494655ce4fb7be1
Michael Bai <mbai@nd.edu>
2022-10-05 23:56:33 -0400

    challenge11: done

--- challenge11/program.py
+++ challenge11/program.py
@@ -1,27 +1,27 @@
 import sys
 
 def min_transactions(value):
-   cache = {}
+    cache = {}
    
    # base cases
-   cache[1] = 1
-   cache[2] = 2
+    cache[1] = 1
+    cache[2] = 2
    
    # prepopulate table
-   for i in range(3, value + 1):
-       cache[i] = 1
+    for i in range(3, value + 1):
+        cache[i] = 1
 
-   for i in range(1, value + 1):
-       if i < 2:
-           continue
+    for i in range(1, value + 1):
+        if i < 2:
+            continue
        # if value of fortnite card is even, we can get to it through doubling half that value stored in the cache
-       if i % 2 == 0:
-           cache[i] = cache[i // 2] + 1 
+        if i % 2 == 0:
+            cache[i] = cache[i // 2] + 1 
        # else, we must get to it by adding one to the value to the left
-       else:
-           cache[i] = cache[i - 1] + 1
-           
-   return cache[value]
+        else:
+            cache[i] = cache[i - 1] + 1   
+    # return the last value in the table (we build up to that value)
+    return cache[value]
 
 def main():
     cases = []
