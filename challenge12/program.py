2bf23120d9d78c93ca2f7a3a469c142fd168d5cf
Michael Bai <mbai@nd.edu>
2022-10-08 16:32:59 -0400

    challenge12: done

--- challenge12/program.py
+++ challenge12/program.py
@@ -12,30 +12,30 @@ def read_grid(n):
 def min_kakamora(grid, n):
     table = [[sys.maxsize for _ in range(n + 1)] for _ in range(n + 1)]
     
-    for r in range(1, n + 1):
-        for c in range(1, n + 1):
+    for row in range(1, n + 1):
+        for col in range(1, n + 1):
             # update table based on the value of the square + min(up, left, up-left)
-            table[r][c] = min(table[r-1][c], table[r][c-1], table[r-1][c-1]) + grid[r][c]
+            table[row][col] = min(table[row-1][col], table[row][col-1], table[row-1][col-1]) + grid[row][col]
             table[1][1] = grid[1][1]
             
     return table
 
 def find_path(grid, n, table):
     path = []
-    r, c = n, n 
-    while r > 0 and c > 0:
-        path.append(grid[r][c])
+    row, col = n, n 
+    while row > 0 and col > 0:
+        path.append(grid[row][col])
         
         # backtrack left
-        if table[r][c] - grid[r][c] == table[r][c - 1]:
-            c -= 1
+        if table[row][col] - grid[row][col] == table[row][col - 1]:
+            col -= 1
         # backtrack up
-        elif table[r][c] - grid[r][c] == table[r - 1][c]:
-            r -= 1
+        elif table[row][col] - grid[row][col] == table[row - 1][col]:
+            row -= 1
         # backtrack up-left
         else:
-            r -= 1
-            c -= 1
+            row -= 1
+            col -= 1
     path.reverse()
     
     return path
@@ -43,20 +43,20 @@ def find_path(grid, n, table):
 def main():
     while True:
        
-       try: 
+        try: 
             n = int(sys.stdin.readline())
             # signifies end of input
             if n == 0:
                 break
-       except ValueError:
-           break
+        except ValueError:
+            break
        
-       grid = read_grid(n)
-       table = min_kakamora(grid, n)
-       path = find_path(grid, n, table)
+        grid = read_grid(n)
+        table = min_kakamora(grid, n)
+        path = find_path(grid, n, table)
        
-       print(table[n][n])
-       print(' '.join(str(c) for c in path))
+        print(table[n][n])
+        print(' '.join(str(element) for element in path))
     
 if __name__ == '__main__':
     main()
