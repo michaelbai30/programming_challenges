bba2dca1ab997640128b1f482bbfe4a548078367
Michael Bai <mbai@nd.edu>
2022-10-29 21:09:03 -0400

    challenge14: done

--- challenge14/program.py
+++ challenge14/program.py
@@ -1,7 +1,8 @@
 import sys
 from collections import deque
-global counter
-counter = 0
+global COUNTER
+COUNTER = 0
+
 class Node:
     def __init__(self, val, left=None, right=None):
         self.val = val
@@ -9,60 +10,53 @@ class Node:
         self.right = right
 
 # create Binary Tree recursively given list of nodes
-def create_BCT(nodes, index):
+def create_bct(nodes, index):
     if index >= len(nodes):
         return None
     
     root = Node(nodes[index]) 
-    root.left = create_BCT(nodes, 2 * index + 1)
-    root.right = create_BCT(nodes, 2 * index + 2) 
+    root.left = create_bct(nodes, 2 * index + 1)
+    root.right = create_bct(nodes, 2 * index + 2) 
 
     return root
 
 # helper function for recursive DFS
 def helper(root, path, target):
-    global counter
+    global COUNTER
     if root is None:
         return
     
     path.append(root.val)
-    if path[-len(target): ] == target:
-    
-            #print(f'curpath --- {path[-len(target):]}')
-            #print('yes')
-
-        counter += 1
-    
     helper(root.left, path, target)
     helper(root.right, path, target)
     
-    #print(f'path --- {path}')
-   
-    
-    path.pop()
-    
+    # counts the last N elements of path and compares to target
+    if path[-len(target): ] == target: 
+        COUNTER += 1
+        
+    if path: 
+        path.pop()
     
 def dfs(root, target):
-    global counter
+    global COUNTER
     helper(root, [], target)
     
-    return counter
+    return COUNTER
 
 def main():
-    
-   cases = []
-   for line in sys.stdin:
-       cases.append([int(x) for x in line.strip().split()]) 
+    cases = []
+    for line in sys.stdin:
+        cases.append(line.strip().split())
        
-   for case in cases:
-        global counter
-        binary = "{0:b}".format(case[0])
+    for case in cases:
+        global COUNTER
+        binary = "{0:b}".format(int(case[0]))
         binary_arr = [int(x) for x in str(binary)]
-        nodes = [int(x) for x in str(case[1])]
-        count = dfs(create_BCT(nodes, 0), binary_arr)
+        nodes = [int(x) for x in case[1]]
+        count = dfs(create_bct(nodes, 0), binary_arr)
         print(f"Paths that form {case[0]} in binary ({binary}): {count}")
-        counter = 0 
-        
-        
+        # reset counter after each case
+        COUNTER = 0 
+
 if __name__ == '__main__':
     main()
