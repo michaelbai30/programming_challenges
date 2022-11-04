b8a97777862d4bfebe48fbcff9339252c792dabc
Michael Bai <mbai@nd.edu>
2022-11-04 11:48:41 -0400

    challenge15: done

--- challenge15/program.py
+++ challenge15/program.py
@@ -1,50 +1,50 @@
 import sys
 import collections
 
-def read_graph(n, m):
-    g = collections.defaultdict(list)
+def read_graph(num_edges):
+    graph = collections.defaultdict(list)
     
-    for _ in range(m):
+    for _ in range(num_edges):
         source, target = map(int, sys.stdin.readline().split())
-        g[source].append(target)
-        g[target].append(source)
+        graph[source].append(target)
+        graph[target].append(source)
         
-    return g
+    return graph
 
-def walk(g, v, prev, visited, longest):
+def walk(graph, vertex, prev, visited, longest):
     longest = 0
    
     # if edge has already been visited 
-    if (prev, v) in visited or (v, prev) in visited:
+    if (prev, vertex) in visited or (vertex, prev) in visited:
         return 0
     
-    for neighbor in g[v]:
+    for neighbor in graph[vertex]:
         # add edge to visited    
-        visited.add((v, prev))
-        visited.add((prev,v))
+        visited.add((vertex, prev))
+        visited.add((prev,vertex))
         # if new edge
-        longest = max(walk(g, neighbor, v, visited, longest) + 1, longest)
+        longest = max(walk(graph, neighbor, vertex, visited, longest) + 1, longest)
         # called once the we pop out of the recursive call
-        visited.remove((v,prev))
-        visited.remove((prev,v)) 
+        visited.remove((vertex,prev))
+        visited.remove((prev,vertex)) 
     return longest     
 
 def main():
-    n, m = map(int, sys.stdin.readline().split())
-    while n and m:
+    num_nodes, num_edges = map(int, sys.stdin.readline().split())
+    while num_nodes and num_edges:
         
         max_longest = 0  
-        g = read_graph(n, m)
+        graph = read_graph(num_edges)
         
         # iterate from each vertex in the graph
         # to get longest path
-        for v in g.keys():
-            max_longest = max(max_longest, walk(g, v, -1, set(), 0))
+        for vertex in graph.keys():
+            max_longest = max(max_longest, walk(graph, vertex, -1, set(), 0))
         
         # result
         print(max_longest - 1)
         
-        n, m = map(int, sys.stdin.readline().split())
+        num_nodes, num_edges = map(int, sys.stdin.readline().split())
     
 if __name__ =='__main__':
     main()
